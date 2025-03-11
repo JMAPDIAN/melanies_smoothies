df9ecb16b6632552f3a437dd70115920d2e2fadc
JMAPDIAN <142478506+JMAPDIAN@users.noreply.github.com>
2025-03-11 15:25:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -12,14 +13,14 @@ st.write("The name on smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col("FRUIT_NAME"))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 # Convert the Snowpark Dataframe to Pandas Dataframe to use the LOC function
 pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
-    pd_df["FRUIT_NAME"].tolist(),
+    my_dataframe,
     max_selections=5
 )
 
@@ -29,12 +30,14 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        
         st.subheader(fruit_chosen + " Nutrition Information")
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    my_insert_stmt = f"""INSERT INTO smoothies.public.orders(ingredients, name_on_order)
-                        VALUES ('{ingredients_string}', '{name_on_order}')"""
+    my_insert_stmt = f""" insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('{ingredients_string}', '{name_on_order}')"""
     
     time_to_insert = st.button('Submit Order')
 
